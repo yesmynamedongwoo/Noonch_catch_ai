127cbc578d362c37db9ed51c8a3dff998ad51a30
yesmynamedongwoo <kdw3917ss@gmail.com>
2022-02-28 22:25:46 +0900

    ai_눈치케치 완료

--- Noonch_catch_ai/urls.py
+++ Noonch_catch_ai/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from ninja import NinjaAPI
+
+from nstapp.apis.v1.nst_router import router as nst_router
+
+api = NinjaAPI()
+api.add_router("/nsts/", nst_router)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", api.urls),
+    path("", include('crawling.urls'))
+
 ]
